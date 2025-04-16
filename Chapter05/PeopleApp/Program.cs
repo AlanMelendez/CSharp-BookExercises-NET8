7de7d196b26df9ab022c6ec0a04c7b6c56f1e089
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-04-15 23:18:49 -0600

    Setting and outputting field values

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -18,3 +18,18 @@ WriteLine(bob); // Implicit call to ToString().
 // WriteLine(bob.ToString()); // Does the same thing.
 
 #endregion
+
+#region Setting and outputting field values
+
+bob.Name = "Alan Melendez";
+bob.Born = new DateTimeOffset(
+  year: 1985, month: 4, day: 11,
+  hour: 16 , minute: 28, second: 0,
+  offset: TimeSpan.FromHours(-5) // UTC-5
+); // DateTimeOffset is a struct, so it can't be null.
+
+WriteLine(format: "{0} was born on {1:D}.", arg0: bob.Name, arg1: bob.Born); // D means long date pattern. (it's similar to use a pipe in Angular)
+WriteLine($"{bob.Name} was born on {bob.Born:D}."); // Interpolated string.
+WriteLine($"{bob.Name} was born on {bob.Born:yyyy-MM-dd}."); // Interpolated string with format.
+
+#endregion
